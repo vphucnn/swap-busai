d39b03204de50a53fe6fc0c76260742d63f88401
vphucnn <140361269+vphucnn@users.noreply.github.com>
2024-06-18 15:00:17 +0700

    edit style chip

--- src/@core/components/chip/BusAiChip.tsx
+++ src/@core/components/chip/BusAiChip.tsx
@@ -18,10 +18,14 @@ export const BusAiChip: React.FC<BusAiChipProps> = ({ label, showRemove, remove,
           color: '#111111', // Custom text color
           borderRadius: 5, // Custom border radius
           fontSize: 16, // Custom font size
-          fontWeight: 400, // Custom font weight
+          fontWeight: 400,
+
+           // Custom font weight
           '& .MuiChip-deleteIcon': { // Target delete icon styles
             color: 'grey',
-          }
+          },
+          '& .MuiChip-label': { whiteSpace: 'normal' },
+          height: 'auto'
         }}
         {...props}
       />
