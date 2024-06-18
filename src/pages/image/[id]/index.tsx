0629e002d0fef7b53786848984f4ae8b949f6aa5
vphucnn <140361269+vphucnn@users.noreply.github.com>
2024-06-18 13:32:41 +0700

    add Helmet

--- src/pages/image/[id]/index.tsx
+++ src/pages/image/[id]/index.tsx
@@ -2,31 +2,41 @@ import { Box } from '@mui/system';
 import axios from 'axios';
 import { ReactNode } from 'react';
 import BlankLayout from 'src/@core/layouts/BlankLayout';
+import Helmet from 'react-helmet'; // Or import { MetaTags } from 'react-meta-tags';
 
-const Image = ({ imageData }: any) => (
+const Image = ({ imageData, id }: any) => (
+  <>
+    <Helmet>
+      <meta property="og:title" content={id} />
+      <meta property="og:description" content={id} />
+      <meta property="og:image" content={`data:image/jpeg;base64,${imageData}`} />
+      <meta property="og:url" content={window.location.href} />
+      <meta property="og:type" content="website" />
+    </Helmet>
 
-  <Box sx={{ display: 'flex' }}>
-    {imageData? <img alt="" style={{ margin: 'auto' }} src={`data:image/jpeg;base64,${imageData}`} /> : <p>Image not found</p>}
-  </Box >
+    <Box sx={{ display: 'flex' }}>
+      {imageData ? <img alt="" style={{ margin: 'auto' }} src={`data:image/jpeg;base64,${imageData}`} /> : <p>Image not found</p>}
+    </Box >
+  </>
 );
 
 export const getServerSideProps = async (context: any) => {
   // Fetch the image from the external URL
-  try{
+  try {
 
-  console.log("slug", context.params.id)
+    console.log("slug", context.params.id)
 
-  const response = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN_API}/api/ai/image/${context.params.id}.png`, {
-    responseType: 'arraybuffer',
-  });
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_DOMAIN_API}/api/ai/image/${context.params.id}.png`, {
+      responseType: 'arraybuffer',
+    });
 
-  // Convert the image data to base64
-  const imageData = Buffer.from(response.data, 'binary').toString('base64');
+    // Convert the image data to base64
+    const imageData = Buffer.from(response.data, 'binary').toString('base64');
 
-  return { props: { imageData } };
-}catch{
-  return { props: {imageData : null} }
-}
+    return { props: { imageData , id: context.params.id } };
+  } catch {
+    return { props: { imageData: null, id: null } }
+  }
 };
 
 export default Image;
