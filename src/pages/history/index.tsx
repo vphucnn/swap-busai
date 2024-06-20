86321fffc990dde656e780a6f1c16b1a52d4e525
vphucnn <140361269+vphucnn@users.noreply.github.com>
2024-06-20 10:50:58 +0700

    add back button

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -7,10 +7,11 @@ import Box from '@mui/material/Box';
 // import { styled } from '@mui/material/styles';
 
 // import NProgress from 'nprogress'
-import { Typography } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 
 // import { CopyIcon } from 'src/views/components/copy-icon/CopyIcon';
 import TableHistory from 'src/views/table/mui/TableHistory';
+import { Icon } from '@iconify/react';
 
 // const Img = styled('img')(({ theme }) => ({
 //   maxWidth: "100%",
@@ -29,6 +30,11 @@ export default function Page() {
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', gap: '1rem', marginTop: '3rem' }}>
+      <Box sx={{ width: '100%', padding: '0.5rem', maxWidth: '1350px' , display: 'flex', justifyContent: 'start'}}>
+        <Link href='/' sx={{display: 'flex', backgroundColor: '#0F0BC1', padding: '2px', borderRadius: '100%', width: '50px', height: "50px", alignItems: 'center', justifyContent: 'center'}}>
+          <Icon icon='icon-park-solid:back' fontSize={30} />
+        </Link>
+      </Box>
       <Box>
         <Typography sx={{ typography: { lg: 'h1', xs: 'h3' } }}>
           History
